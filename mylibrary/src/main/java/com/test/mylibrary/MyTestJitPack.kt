1d0f43a05b6ec171bfd9b52365eaf812733f6de9
lidexin <1244300706@qq.com>
2021-07-06 09:55:12 +0800

    提交lib库

---
+++ mylibrary/src/main/java/com/test/mylibrary/MyTestJitPack.kt
@@ -0,0 +1,17 @@
+package com.test.mylibrary
+
+import android.util.Log
+
+/**
+ *
+ *@author lidexin
+ *@email tuz1425@dingtalk.com
+ *@date 2021/7/6
+ */
+object MyTestJitPack {
+
+    fun printLogMessage() {
+        Log.d("MyTestJitPack", "test log")
+    }
+
+}
